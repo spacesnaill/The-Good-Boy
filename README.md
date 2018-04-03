40697f724eb34e533db50a763e09902385cd1d0e
PatG <patrickjgeiger@gmail.com>
2018-04-02 20:38:46 -0400

    Updated the read me with a link to Google Calendar API

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # The-Good-Boy
 A discord bot meant to post pictures and videos of dogs, along with some other operations.
 
-Current dependencies:
+Current dependencies(not an exhaustive list, but these are the major ones):
 
 Tweepy (http://docs.tweepy.org/en/latest/)
 
 Discord.py (http://discordpy.readthedocs.io/en/latest/api.html)
+
+Google Calendar API (https://developers.google.com/calendar/)
--- TweetCollector.py
+++ TweetCollector.py
@@ -3,9 +3,8 @@ import tweepy
 class TweetCollector:
 
     #Set up to use the twitter api through Tweepy
-    auth = tweepy.OAuthHandler('djOUfbU4YXBoDZL0ZeLnPqGX6', 'shSx1lVShE0wrZ6rDURQhtORNoCFhQYqwWnQYhyL4M9CMljqCv')
-    auth.set_access_token('3044253956-GqacM6MLHi3zuAkqhBTzaHkJCXQdDm1ZSPzC8RN',
-                          'BVIbFYWck2oB9MOqmLqxzcowHEoTCVmP2tItbh1HiEYFd')
+    auth = tweepy.OAuthHandler('','')
+    auth.set_access_token('','')
     tweepy_api = tweepy.API(auth)
 
     def __init__(self, twitterID):
